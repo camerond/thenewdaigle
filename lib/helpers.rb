711d71e0059059071ec0e10955c4153a47fbce0d
Cameron Daigle <cameron@hashrocket.com>
2010-09-06 21:26:29 -0400

    Add next and previous post links

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,4 +1,35 @@
+def has_previous_article?(article)
+  sorted_articles.index(article) + 1 < sorted_articles.size
+end
 
+def has_next_article?(article)
+  sorted_articles.index(article) > 0
+end
+
+def previous_articles(article, n=1)
+  @sorted_articles ||= sorted_articles
+  index = @sorted_articles.index(article)
+  last = @sorted_articles.size - 1
+  if index >= last
+    []
+  elsif index + n <= last
+    @sorted_articles[index+1, n]
+  else
+    @sorted_articles[index+1, last - index]
+  end
+end
+
+def next_articles(article, n=1)
+  @sorted_articles ||= sorted_articles
+  index = @sorted_articles.index(article)
+  if index <= 0
+    []
+  elsif index - n >= 0
+    @sorted_articles[index-n, n]
+  else
+    @sorted_articles[0, index]
+  end
+end
 
 # Tag methods yoinked from Sinatra methods and slightly modified
 
